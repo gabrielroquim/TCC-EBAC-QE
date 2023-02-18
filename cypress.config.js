b6f20a1fda1609d1b38a40f52d8297f4b7479b81
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-18 10:26:53 -0300

    inserindo id projeto

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  projectId: 'x3oxe3',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br",
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
