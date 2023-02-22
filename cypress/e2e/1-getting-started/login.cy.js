41374cc3658e56c96398feadf4c84d0b626bbb86
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-22 18:02:06 -0300

    login ok

--- cypress/e2e/1-getting-started/login.cy.js
+++ cypress/e2e/1-getting-started/login.cy.js
@@ -1,10 +1,27 @@
 ///<reference types="cypress" />
 
-describe('Login EBAC shop', () => {
-  beforeEach(() =>{
-    cy.visit('/minha-conta/')
-  })
-  it('Login válido', () => {
-    
+let dadosLogin
+
+context('Funcionalidade Login', () => {
+  before(() => {
+    cy.fixture('perfil').then(perfil => {
+      dadosLogin = perfil
+    })
   });
-});
+
+  beforeEach(() => {
+    cy.visit('minha-conta')
+  });
+
+  afterEach(() => {
+    cy.screenshot()
+  })
+
+  it('Login usando fixture', () => {
+    cy.fixture('perfil').then((dados) => {
+      cy.login(dados.username, dados.password)
+    })
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', ' gabsqa-1779')
+  });
+})
