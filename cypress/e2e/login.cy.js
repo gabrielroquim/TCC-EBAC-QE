bd088df0986e71bd870b5ca4b1bb0587bd76b1a4
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-22 18:38:45 -0300

    instalando mochawesome

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -17,11 +17,19 @@ context('Funcionalidade Login', () => {
     cy.screenshot()
   })
 
-  it('Login usando fixture', () => {
+  it('Login com sucesso', () => {
     cy.fixture('perfil').then((dados) => {
       cy.login(dados.username, dados.password)
     })
     cy.get('.page-title').should('contain', 'Minha conta')
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', ' gabsqa-1779')
+  });
+
+  it('Login sem sucesso', () => {
+    cy.fixture('perfil').then((dados) => {
+      cy.login(dados.username2, dados.password2)
+    })
+    cy.get('.page-title').should('contain', 'Minha conta') 
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
   });
 })
