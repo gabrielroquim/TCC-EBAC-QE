cbcd5ac224e80ae3de0ee008eab090af24d66ac4
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-23 21:28:41 -0300

    alterando jack

--- api/integration/login.spec.js
+++ api/integration/login.spec.js
@@ -6,6 +6,24 @@ describe('Login - API CUPOM SHOP', () => {
     before(() => {
         cy.token('admin_ebac', '@admin!&b@c!2022').then(tkn => { token = tkn })  // vai usar o commands, criei uma variavel tkn , ela vai reeber o token
     });
+    
+    it('Realizando POST de cupom já existente', () => {
+        request('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
+            .post('/coupons')
+            .send({
+                "code": "nomeCupom",
+                "amount": "10",
+                "discount_type": "fixed_product",
+                "description": "Cupom de desconto de teste",
+            })
+            .set('Accept', 'application/json')
+            .set("Authorization", JSON.stringify(authorization))
+            .then(response => {
+                //expect(response.body.data.status).toEqual(400)
+                expect(response.status).toEqual(400)
+                expect(response.body.message).toEqual('O código de cupom já existe')
+            })
+    });
 
     it('Cadastrar Cupom', () => {
         let produto = `QAEBAC ${Math.floor(Math.random() * 100000)}` //esta usando uma funça do javascript para gerar um numero randomico, pois assim podemnos cadastrar varios prodtuso com numeros diferesntes
--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/api/integration/*.spec.js',
     baseUrl: "http://lojaebac.ebaconline.art.br",
     setupNodeEvents(on, config) {
       allureWriter(on, config);
