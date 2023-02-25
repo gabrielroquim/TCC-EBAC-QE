926cea04a36a5d92e43635e7d816d7104aeee786
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-25 17:04:18 -0300

    api

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,10 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
-    //specPattern: 'cypress/api/integration/*.cy.js',
-    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
+   specPattern: 'api/integration/*.spec.js',
     baseUrl: "http://lojaebac.ebaconline.art.br",
+   // specPattern: 'tcc-ebac-qe/api/integration/*.spec.js',
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       allureWriter(on, config);
       return config;
