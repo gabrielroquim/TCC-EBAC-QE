383e64b98b251aba1563c9f21f23e0d95d4b0acb
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-25 19:32:52 -0300

    string to be

--- api/integration/loginAPI.spec.js
+++ api/integration/loginAPI.spec.js
@@ -1,19 +1,18 @@
 const req = require('supertest');
-const authorization = require('../utils/utils.json')
-c//onst ApiUrl = "http://lojaebac.ebaconline.art.br/wp-json/wc/v3";
+const authorization = require('../utils/token.json')
 
 
-describe('API de cupons', () => {
-    beforeAll( async() => {
-        
-    })
-    it('Realizando um GET de cupons', () => {
-        request('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
+
+describe('API de cupons', () => { 
+let token = "Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
+    it('Realizando um GET de cupons', async () => {
+      return req('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
             .get('/coupons')
             .set('Accept', 'application/json')
-            .set("Authorization", JSON.stringify(authorization))
+            .set("Authorization", JSON.stringify(token))
             .then(response => {
                 expect(response.status).toEqual(200)
+                expect(response.body.id).not.toBe(toString);
             })
     });
 })
