b7c1f57b3c3ee63f21cd823f9b017aa8a935ff9e
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-26 20:56:37 -0300

    mudando o login UI para dar errro

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -19,23 +19,21 @@ context('Funcionalidade Login', () => {
 
   it('Login com sucesso', () => {
     cy.fixture('perfil').then((dados) => {
-      cy.login(dados.username, dados.password)
+    cy.login(dados.username, dados.password)
     })
     cy.get('.page-title').should('contain', 'Minha conta')
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', ' gabsqa-1779')
   });
 
-  it('Login com senha inválida', () => {
-    cy.fixture('perfil').then((dados) => {
-      cy.login(dados.username2, dados.password2)
-    })
-    cy.get('.page-title').should('contain', 'Minha conta')
-    cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
+  it('Login sem preencher E-mail', () => {
+    cy.get('#password').type('223233')
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: Nome de usuário é obrigatório.')  
   });
 
   it('Login com email inexistente', () => {
     cy.fixture('perfil').then((dados) => {
-    cy.login(dados.username3, dados.password3)    
+    cy.login(dados.username3)    
     })
     cy.get('.page-title').should('contain', 'Minha conta')
     cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
