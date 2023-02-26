643c0b9e01d4050826fb834887ca794b1b6cc61c
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-26 20:48:13 -0300

    mudando link cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -25,11 +25,28 @@ context('Funcionalidade Login', () => {
     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', ' gabsqa-1779')
   });
 
-  it('Login sem sucesso', () => {
+  it('Login com senha inválida', () => {
     cy.fixture('perfil').then((dados) => {
       cy.login(dados.username2, dados.password2)
     })
-    cy.get('.page-title').should('contain', 'Minha conta') 
+    cy.get('.page-title').should('contain', 'Minha conta')
     cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
   });
+
+  it('Login com email inexistente', () => {
+    cy.fixture('perfil').then((dados) => {
+    cy.login(dados.username3, dados.password3)    
+    })
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
+  });
+
+  it('Login com email não preenchido', () => {
+    cy.fixture('perfil').then((dados) => {
+    cy.login(dados.password3)    
+    })
+    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-error > li').should('contain', 'Erro: a senha fornecida para o e-mail gabsqa@testesebac.com.qa está incorreta. Perdeu a senha?')
+  });
+
 })
