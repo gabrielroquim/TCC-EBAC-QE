a616ad8293dc664da49f6f4381e21a425fe970f6
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-25 22:25:02 -0300

    jest

--- api/integration/loginAPI.spec.js
+++ api/integration/loginAPI.spec.js
@@ -3,16 +3,43 @@ const authorization = require('../utils/token.json')
 
 
 
-describe('API de cupons', () => { 
-let token = "Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
-    it('Realizando um GET de cupons', async () => {
-      return req('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
+describe('API de cupons Loja EBAC', () => {
+    let token = "Basic YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy"
+
+    it.only('Realizando um GET de cupons', async () => {
+
+        return req('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
             .get('/coupons')
             .set('Accept', 'application/json')
             .set("Authorization", JSON.stringify(token))
             .then(response => {
-                expect(response.status).toEqual(200)
-                expect(response.body.id).not.toBe(toString);
+                expect(response.status).toBe(200)
+                expect(response.body).toBeDefined()
+                expect(response.body).toBeInstanceOf(Array)            
+                expect(response.body.code).toBe(undefined)
+                const  ler = response.body
+                expect([ler]).toMatchObject([
+                    {foo: 'bar'},
+                    {baz: 1},
+                ])
+            })
+    });
+
+    it('Cadastrando CUPOM - POST', () => {
+        req('http://lojaebac.ebaconline.art.br/wp-json/wc/v3')
+            .post('/coupons')
+            .send({
+                "code": "Sonic2022",
+                "amount": "10",
+                "discount_type": "fixed_product",
+                "description": "Cupom de desconto de teste",
+            })
+            .set('Accept', 'application/json')
+            .set("Authorization", JSON.stringify(authorization))
+            .then(response => {
+                //expect(response.body.data.status).toEqual(400)
+                expect(response.status).toEqual(400)
+                expect(response.body.message).toEqual('O código de cupom já existe')
             })
     });
 })
