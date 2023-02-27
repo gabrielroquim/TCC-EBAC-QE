c171db2ceba1898713551b97d198bbd94fe80c71
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-02-27 19:59:45 -0300

    contrato sendo feito

--- api/Contratos/contratos.spec.js
+++ api/Contratos/contratos.spec.js
@@ -1,6 +1,7 @@
-import { Pact } from "@pact-foundation/pact" 
+import { Pact } from "@pact-foundation/pact"
+import { eachLike, somethingLike } from "@pact-foundation/pact/src/dsl/matchers"
 import 'dotenv/config'
-import { resolve} from 'path'
+import { resolve } from 'path'
 const mockProvider = new Pact({
     consumer: 'coupons-admin',
     provider: 'coupons-client',
@@ -11,12 +12,75 @@ const mockProvider = new Pact({
 })
 
 describe(' Consumer Test', () => {
-    beforeAll(() =>{
+    beforeAll(async () => {
+        await mockProvider.setup().then(() => {
+            mockProvider.addInteraction({
+                uponReceiving: 'a request',
+                withRequest: {
+                    method: 'GET',
+                    path: '/coupons',
+                    headers: {
+                        Authorization: 'Bearer YWRtaW5fZWJhYzpAYWRtaW4hJmJAYyEyMDIy',
+                        "Content-Type": 'aplication/json'
+                    },
+                    body: {
 
+                    }
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        "Content-Type": 'aplication/json; charset=UTF-8'
+                    },
+                    body:
+                        eachLike(
+                            {
+                                "id": somethingLike(8829),
+                                "code": somethingLike("vonz"),
+                                "amount": somethingLike("20.00"),
+                                "date_created": somethingLike("2023-02-25T18:55:47"),
+                                "date_created_gmt": somethingLike("2023-02-25T21:55:47"),
+                                "date_modified":somethingLike("2023-02-25T18:55:47"),
+                                "date_modified_gmt": somethingLike("2023-02-25T21:55:47"),
+                                "discount_type": somethingLike("fixed_product"),
+                                "description": somethingLike("Cupom reservea"),
+                                "date_expires": somethingLike(null),
+                                "date_expires_gmt": somethingLike(null),
+                                "usage_count":  somethingLike(0),
+                                "individual_use": false,
+                                "product_ids": [],
+                                "excluded_product_ids": [],
+                                "usage_limit": null,
+                                "usage_limit_per_user": null,
+                                "limit_usage_to_x_items": null,
+                                "free_shipping": false,
+                                "product_categories": [],
+                                "excluded_product_categories": [],
+                                "exclude_sale_items": false,
+                                "minimum_amount": "0.00",
+                                "maximum_amount": "0.00",
+                                "email_restrictions": [],
+                                "used_by": [],
+                                "meta_data": [],
+                                "_links": {
+                                    "self": [
+                                        {
+                                            "href": "http:\/\/lojaebac.ebaconline.art.br\/wp-json\/wc\/v3\/coupons\/8829"
+                                        }
+                                    ],
+                                    "collection": [
+                                        {
+                                            "href": "http:\/\/lojaebac.ebaconline.art.br\/wp-json\/wc\/v3\/coupons"
+                                        }
+                                    ]
+                                }
+                            }),
+                }
+            })
+        })
     })
 
     it('should coupons', () => {
-        
+
     });
 });
-    
