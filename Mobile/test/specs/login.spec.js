7637f619d0c8143aa6697471228b7cc404efbcb0
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-03-18 16:45:56 -0300

    alterando projeto

--- Mobile/test/specs/login.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Access Admin Panel', () => {
-    it('should login with valid credentials', async () => {
-        setTimeout(() => {
-            console.log('abriu')
-        }, 10000)
-    });
-});
