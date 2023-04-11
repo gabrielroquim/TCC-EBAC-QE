eb0693428d00375f7a58386cbc54da2954c03d26
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-04-11 20:07:25 -0300

    subindo

--- UI/cypress/cypress.config.js
+++ UI/cypress/cypress.config.js
@@ -1,9 +1,29 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  projectId: 'x3oxe3',
   e2e: {
+    //specPattern: 'cypress/api/integration/*.cy.js',
+    specPattern: 'cypress/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureWriter(on, config);
+      return config;
+    },
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      configFile: 'reporter-config.json',
+    },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: true,
+      json: true,
     },
   },
+  
+
 });
